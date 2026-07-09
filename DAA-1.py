84e389e1c4b669e4a7e208e1101b90993f11b046
Divya Dharshini <divimano2007@gmail.com>
2026-07-09 23:50:30 +0530

    Add files via upload

---
+++ DAA-1.py
@@ -0,0 +1,16 @@
+import time
+import matplotlib.pyplot as plt
+import random
+def linear_search(arr, x):
+for i in range(len(arr)):
+if arr[i] == x:
+return ireturn -1
+n_values = [100, 1000, 10000, 100000, 1000000]
+time_values = []
+for n in n_values:
+()linear_search(arr, x) end_time = time.time()
+time_values.append(end_time - start_time)
+plt.plot(n_values, time_values)plt.title('Linear Search')
+plt.xlabel('Number of Elements')
+plt.ylabel('Time Taken (seconds)')
+plt.show()
